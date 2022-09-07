dd29e6764bf10f75df4a46e096d6a30ddfc57b7c
rubensancor <ruben.sanchez@deusto.es>
2022-09-07 12:38:33 +0200

    v2

--- react/src/components/dashboard/tree/PhaseTabs.js
+++ react/src/components/dashboard/tree/PhaseTabs.js
@@ -1,8 +1,19 @@
 import { AppBar, LinearProgress, Stack, Tab, Tabs as MuiTabs } from "@material-ui/core";
 import React from "react";
 import { TreeItemTypeChip } from "../assets/Icons";
+import { Button } from "@material-ui/core";
+import { Add } from '@material-ui/icons';
+import TreeItemCreate from "./TreeItemCreate";
+
 
 const PhaseTabs = ({ t, loading = false, selectedId, phases, onSelect }) => {
+    const [treeItemCreatorOpen, setTreeItemCreatorOpen] = React.useState(false);
+    const [treeItemLoading, setTreeItemLoading] = React.useState(false);
+
+    const onItemCreate = (res) => {
+        
+      }
+
     return (
         <AppBar position="static" sx={{ color: "white" }}>
             <MuiTabs
@@ -17,16 +28,25 @@ const PhaseTabs = ({ t, loading = false, selectedId, phases, onSelect }) => {
                 {phases.map((phase) => !phase.is_disabled && (
                     <Tab
                         key={phase.id}
-                        label={<Stack sx={{my: 1}}>
-                        {phase.name}
-                        {false && <TreeItemTypeChip treeitem={phase} t={t} sx={{mt: 1}} />}
-                        
+                        label={<Stack sx={{ my: 1 }}>
+                            {phase.name}
+                            {false && <TreeItemTypeChip treeitem={phase} t={t} sx={{ mt: 1 }} />}
+
                             {/* <CircularProgressWithLabel value={phase.progress} size={40} sx={{ mb: 2 }} />*/}</Stack>}
                         value={phase.id}
                     />
                 ))}
+                {/* <Button onClick={() => setCoproductionProcessCreatorOpen(true)} sx={{ my: 3, width: 400 }} variant="contained" size="small">{t("Create a new co-production process")}</Button> */}
+                <Button onClick={() => setTreeItemCreatorOpen(true)} variant="link" startIcon={<Add />}></Button>
             </MuiTabs>
             {loading && <LinearProgress />}
+            <TreeItemCreate
+                open={treeItemCreatorOpen}
+                setOpen={setTreeItemCreatorOpen}
+                loading={treeItemLoading}
+                setLoading={setTreeItemLoading}
+                onCreate={onItemCreate}
+              />
         </AppBar>
     );
 };
