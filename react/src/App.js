91b9beed9df7f673c96265cb3273e3812038f6f4
Felipe Vergara <fvergaracl@gmail.com>
2023-03-22 15:41:49 +0100

    eslint config added, sentry for local, dev and demo added

--- react/src/App.js
+++ react/src/App.js
@@ -1,128 +1,145 @@
-import { useMatomo } from '@datapunt/matomo-tracker-react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { Toaster } from 'react-hot-toast';
-import { Navigate, useLocation, useNavigate, useRoutes } from 'react-router-dom';
-import RTL from './components/RTL';
-import SplashScreen from './components/SplashScreen';
-import { PRODUCTION_MODE, REACT_APP_DOMAIN } from './configuration';
-import useAuth from './hooks/useAuth';
-import useScrollReset from './hooks/useScrollReset';
-import useSettings from './hooks/useSettings';
-import routes from './routes/index';
-import './translations/i18n';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProcess, getTree } from 'slices/process';
-import getAssets from './components/dashboard/coproductionprocesses/RightSide'
-import { getCoproductionProcesses } from 'slices/general';
-import { getOrganizations } from 'slices/general';
-import { getUnseenUserNotifications } from 'slices/general';
-
-
+import { useMatomo } from "@datapunt/matomo-tracker-react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useRoutes,
+} from "react-router-dom";
+import RTL from "./components/RTL";
+import SplashScreen from "./components/SplashScreen";
+import { PRODUCTION_MODE, REACT_APP_DOMAIN } from "./configuration";
+import useAuth from "./hooks/useAuth";
+import useScrollReset from "./hooks/useScrollReset";
+import useSettings from "./hooks/useSettings";
+import routes from "./routes/index";
+import "./translations/i18n";
+import { useDispatch, useSelector } from "react-redux";
+import { getProcess, getTree } from "slices/process";
+import getAssets from "./components/dashboard/coproductionprocesses/RightSide";
+import { getCoproductionProcesses } from "slices/general";
+import { getOrganizations } from "slices/general";
+import { getUnseenUserNotifications } from "slices/general";
 
 export const RemoveTrailingSlash = ({ ...rest }) => {
   const location = useLocation();
 
   // If the last character of the url is '/'
-  if (location.pathname.match('/.*/$')) {
+  if (location.pathname.match("/.*/$")) {
     return (
       <Navigate
         replace
         {...rest}
         to={{
-          pathname: location.pathname.replace(/\/+$/, ''),
-          search: location.search
+          pathname: location.pathname.replace(/\/+$/, ""),
+          search: location.search,
         }}
       />
     );
-  } return null;
+  }
+  return null;
 };
-
-
 
 const App = () => {
   const content = useRoutes(routes);
   const { settings } = useSettings();
   const auth = useAuth();
   const { process, selectedTreeItem } = useSelector((state) => state.process);
-  const [socket, setSocket] = useState(null)
-  const [personalSocket, setPersonalSocket] = useState(null)
-  const [lastProcessId, setLastProcessId] = useState(null)
+  const [socket, setSocket] = useState(null);
+  const [personalSocket, setPersonalSocket] = useState(null);
+  const [lastProcessId, setLastProcessId] = useState(null);
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const location = useLocation();
-  const getUuid = require('uuid-by-string');
+  const getUuid = require("uuid-by-string");
 
   useScrollReset();
 
   // Create personal websocket to receive updates on dashboard view
   useEffect(() => {
     //The personal websocket will include when is in organizations
-    if ( ( location.pathname === '/dashboard' || location.pathname === '/dashboard/organizations' ) && auth.isAuthenticated && !personalSocket) {
-
-      let socketProtocol = 'ws:';
-      if (REACT_APP_DOMAIN !== 'localhost') {
-        socketProtocol = 'wss:';
+    if (
+      (location.pathname === "/dashboard" ||
+        location.pathname === "/dashboard/organizations") &&
+      auth.isAuthenticated &&
+      !personalSocket
+    ) {
+      let socketProtocol = "ws:";
+      if (REACT_APP_DOMAIN !== "localhost") {
+        socketProtocol = "wss:";
       }
 
-      const new_socket = new WebSocket(`${socketProtocol}//${REACT_APP_DOMAIN}/coproduction/api/v1/users/${getUuid(auth.user.id)}/ws`);
+      const new_socket = new WebSocket(
+        `${socketProtocol}//${REACT_APP_DOMAIN}/coproduction/api/v1/users/${getUuid(
+          auth.user.id
+        )}/ws`
+      );
 
       new_socket.onopen = () => {
-        console.log('PERSONAL WebSocket Client Connected', new_socket);
+        console.log("PERSONAL WebSocket Client Connected", new_socket);
       };
       new_socket.onmessage = (message) => {
-        console.log("RECEIVING MESSAGE ON PERSONAL SOCKET")
+        console.log("RECEIVING MESSAGE ON PERSONAL SOCKET");
         console.log(message);
         dispatch(getCoproductionProcesses());
-        dispatch(getOrganizations(''));
-        dispatch(getUnseenUserNotifications({'user_id':auth.user.id}));
+        dispatch(getOrganizations(""));
+        dispatch(getUnseenUserNotifications({ user_id: auth.user.id }));
 
         if (window.location.pathname.includes("organizations")) {
-          dispatch(getOrganizations(''));
+          dispatch(getOrganizations(""));
         }
-
       };
-      setPersonalSocket(new_socket)
-    } if ( !(location.pathname === '/dashboard'  || location.pathname === '/dashboard/organizations') && personalSocket) {
-      console.log('PERSONAL WebSocket Client Closed');
+      setPersonalSocket(new_socket);
+    }
+    if (
+      !(
+        location.pathname === "/dashboard" ||
+        location.pathname === "/dashboard/organizations"
+      ) &&
+      personalSocket
+    ) {
+      console.log("PERSONAL WebSocket Client Closed");
       personalSocket.close();
       setPersonalSocket(null);
     }
   }, [location, auth]);
 
   useEffect(() => {
-    const process_changed = !process || process.id !== lastProcessId
+    const process_changed = !process || process.id !== lastProcessId;
     if (process_changed) {
       if (socket) {
-        console.log('WebSocket Client Closed');
+        console.log("WebSocket Client Closed");
         socket.close();
         setSocket(null);
         setLastProcessId(null);
       }
 
       if (process) {
-        console.log(REACT_APP_DOMAIN)
+        console.log(REACT_APP_DOMAIN);
 
-        let socketProtocol = 'ws:';
-        if (REACT_APP_DOMAIN !== 'localhost') {
-          socketProtocol = 'wss:';
+        let socketProtocol = "ws:";
+        if (REACT_APP_DOMAIN !== "localhost") {
+          socketProtocol = "wss:";
         }
 
-        const new_socket = new WebSocket(`${socketProtocol}//${REACT_APP_DOMAIN}/coproduction/api/v1/coproductionprocesses/${process.id}/ws`);
+        const new_socket = new WebSocket(
+          `${socketProtocol}//${REACT_APP_DOMAIN}/coproduction/api/v1/coproductionprocesses/${process.id}/ws`
+        );
         new_socket.onopen = () => {
-          console.log('WebSocket Client Connected', new_socket);
+          console.log("WebSocket Client Connected", new_socket);
         };
         new_socket.onmessage = (message) => {
-          console.log(message)
+          console.log(message);
           // dispatch(getProcess(process.id, false, selectedTreeItem.id ))
         };
-        setSocket(new_socket)
-        setLastProcessId(process.id)
+        setSocket(new_socket);
+        setLastProcessId(process.id);
       }
-
     }
-  }, [process])
+  }, [process]);
 
   // useEffect(() => {
   //   if (!socket && process) {
@@ -146,69 +163,69 @@ const App = () => {
   //   }
   // }, [socket])
 
-
   useEffect(() => {
     if (socket) {
       socket.onmessage = (message) => {
-        const { event, name, extra } = JSON.parse(message.data)
-        console.log(event, extra, name)
+        const { event, name, extra } = JSON.parse(message.data);
+        console.log(event, extra, name);
         if (event.includes("treeitem")) {
           if (event.includes("removed")) {
             if (selectedTreeItem.name === name) {
-              if(selectedTreeItem.prerequisites_ids.length==0){
-                dispatch(getTree(process.id, selectedTreeItem.prerequisites_ids[0]))
-              }else{
-                dispatch(getTree(process.id))
+              if (selectedTreeItem.prerequisites_ids.length == 0) {
+                dispatch(
+                  getTree(process.id, selectedTreeItem.prerequisites_ids[0])
+                );
+              } else {
+                dispatch(getTree(process.id));
               }
-              
             } else {
-              dispatch(getTree(process.id, selectedTreeItem.id))
+              dispatch(getTree(process.id, selectedTreeItem.id));
             }
           } else {
-            dispatch(getTree(process.id, selectedTreeItem.id))
+            dispatch(getTree(process.id, selectedTreeItem.id));
           }
-
-        } else
-          if ((event.includes("phase") || event.includes("objective") || event.includes("task")) && selectedTreeItem) {
-            if (event.includes("removed")) {
-              dispatch(getTree(process.id, selectedTreeItem.prerequisites_ids[0]))
+        } else if (
+          (event.includes("phase") ||
+            event.includes("objective") ||
+            event.includes("task")) &&
+          selectedTreeItem
+        ) {
+          if (event.includes("removed")) {
+            dispatch(
+              getTree(process.id, selectedTreeItem.prerequisites_ids[0])
+            );
+          } else {
+            if (selectedTreeItem) {
+              dispatch(getTree(process.id, selectedTreeItem.id));
             } else {
-              if(selectedTreeItem){
-                dispatch(getTree(process.id, selectedTreeItem.id))
-              }else{
-                dispatch(getTree(process.id))
-              }
-
-              
+              dispatch(getTree(process.id));
             }
-
-          } else if (event.includes("coproductionprocess_removed")) {
-            navigate('/dashboard')
-            // show advertence
-          } else if (event.includes("coproductionprocess") || event.includes("permission")) {
-            dispatch(getProcess(process.id, false, selectedTreeItem.id))
           }
-          else if (event.includes("asset")) {
+        } else if (event.includes("coproductionprocess_removed")) {
+          navigate("/dashboard");
+          // show advertence
+        } else if (
+          event.includes("coproductionprocess") ||
+          event.includes("permission")
+        ) {
+          dispatch(getProcess(process.id, false, selectedTreeItem.id));
+        } else if (event.includes("asset")) {
+          const datosTemp = JSON.parse(message.data);
+          // console.log(datosTemp)
+          // console.log("UPDATE ASSETS")
 
-            const datosTemp = JSON.parse(message.data)
-            // console.log(datosTemp)
-            // console.log("UPDATE ASSETS")
-
-            //Ask if the selected task is the same:
-            if (selectedTreeItem.id === datosTemp.extra.task_id) {
-              dispatch(getTree(process.id, selectedTreeItem.id))
-            } else {
-
-              if (window.location.pathname.includes("overview")) {
-                dispatch(getProcess(process.id, false))
-              }
+          //Ask if the selected task is the same:
+          if (selectedTreeItem.id === datosTemp.extra.task_id) {
+            dispatch(getTree(process.id, selectedTreeItem.id));
+          } else {
+            if (window.location.pathname.includes("overview")) {
+              dispatch(getProcess(process.id, false));
             }
-
-
           }
+        }
       };
     }
-  }, [selectedTreeItem])
+  }, [selectedTreeItem]);
 
   // ANALYTICS
   const { enableLinkTracking, trackPageView } = useMatomo();
@@ -221,12 +238,12 @@ const App = () => {
     <ThemeProvider theme={settings.themeData}>
       <RTL direction={settings.direction}>
         <CssBaseline />
-        <Toaster position='top-center' />
+        <Toaster position="top-center" />
         <Helmet>
           {PRODUCTION_MODE && (
             <meta
-              httpEquiv='Content-Security-Policy'
-              content='upgrade-insecure-requests'
+              httpEquiv="Content-Security-Policy"
+              content="upgrade-insecure-requests"
             />
           )}
         </Helmet>
@@ -234,7 +251,9 @@ const App = () => {
         {auth.isInitialized ? content : <SplashScreen />}
       </RTL>
     </ThemeProvider>
-  ) : <SplashScreen />;
+  ) : (
+    <SplashScreen />
+  );
 };
 
 export default App;
--- react/src/index.js
+++ react/src/index.js
@@ -1,24 +1,46 @@
-import { createInstance, MatomoProvider } from '@datapunt/matomo-tracker-react';
-import StyledEngineProvider from '@mui/material/StyledEngineProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { REACT_APP_COMPLETE_DOMAIN, REACT_APP_MATOMO_ID } from 'configuration';
-import 'nprogress/nprogress.css';
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import { Provider as ReduxProvider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './contexts/CookieContext';
-import { SettingsProvider } from './contexts/SettingsContext';
-import store from './store';
+import { createInstance, MatomoProvider } from "@datapunt/matomo-tracker-react";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { REACT_APP_COMPLETE_DOMAIN, REACT_APP_MATOMO_ID } from "configuration";
+import "nprogress/nprogress.css";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import { Provider as ReduxProvider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./contexts/CookieContext";
+import { SettingsProvider } from "./contexts/SettingsContext";
+import store from "./store";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+let environment = undefined;
+if (REACT_APP_COMPLETE_DOMAIN.includes("localhost")) {
+  environment = "local";
+}
+if (REACT_APP_COMPLETE_DOMAIN.includes("dev")) {
+  environment = "dev";
+}
+if (REACT_APP_COMPLETE_DOMAIN.includes("demo")) {
+  environment = "demo";
+}
+
+if (environment) {
+  Sentry.init({
+    dsn: "https://9c2c4ae5750343deb167d2bdee365657@o4504881526669312.ingest.sentry.io/4504881531256832",
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+    environment,
+  });
+}
 
 const instance = createInstance({
   urlBase: `${REACT_APP_COMPLETE_DOMAIN}/matomo/`,
   siteId: REACT_APP_MATOMO_ID,
-  linkTracking: false
+  linkTracking: false,
 });
 
 ReactDOM.render(
@@ -41,5 +63,5 @@ ReactDOM.render(
       </HelmetProvider>
     </MatomoProvider>
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
