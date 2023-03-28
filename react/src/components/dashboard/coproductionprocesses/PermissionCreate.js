87ad218a8e65f033f95ad926a8a8c292c16d2845
Felipe Vergara <fvergaracl@gmail.com>
2023-03-28 16:31:02 +0200

    update eslintrc.json in the root's project

--- react/src/components/dashboard/coproductionprocesses/PermissionCreate.js
+++ react/src/components/dashboard/coproductionprocesses/PermissionCreate.js
@@ -1,14 +1,35 @@
-import { Alert, Avatar, Box, Button, Card, CardHeader, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, MobileStepper, Stack, Switch, Typography } from '@mui/material';
-import { Add, Close, KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
-import { LoadingButton } from '@mui/lab';
-import useMounted from 'hooks/useMounted';
-import OrganizationsList from 'components/dashboard/organizations/OrganizationsList';
-import TeamCreate from 'components/dashboard/organizations/TeamCreate';
-import TeamsList from 'components/dashboard/organizations/TeamsList';
-import UsersList from 'components/dashboard/organizations/UsersList';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { organizationsApi, permissionsApi } from '__api__';
+import {
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  MobileStepper,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
+import {
+  Add,
+  Close,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+} from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
+import useMounted from "hooks/useMounted";
+import OrganizationsList from "components/dashboard/organizations/OrganizationsList";
+import TeamCreate from "components/dashboard/organizations/TeamCreate";
+import TeamsList from "components/dashboard/organizations/TeamsList";
+import UsersList from "components/dashboard/organizations/UsersList";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { organizationsApi, permissionsApi } from "__api__";
 
 const initial = {
   access_assets_permission: false,
@@ -16,7 +37,12 @@ const initial = {
   delete_assets_permission: false,
 };
 
-const CollapseElement = ({ t, organization, setSelectedTeam, onTeamCreate }) => {
+const CollapseElement = ({
+  t,
+  organization,
+  setSelectedTeam,
+  onTeamCreate,
+}) => {
   const [teamCreatorOpen, setOpenTeamCreator] = useState(null);
   const [creatingTeam, setCreatingTeam] = useState(null);
 
@@ -28,14 +54,14 @@ const CollapseElement = ({ t, organization, setSelectedTeam, onTeamCreate }) => 
         loadingTeams={false}
         onTeamClick={setSelectedTeam}
       />
-      <Box sx={{ textAlign: 'center', mt: 2, mb: 2 }}>
+      <Box sx={{ textAlign: "center", mt: 2, mb: 2 }}>
         <LoadingButton
           startIcon={<Add />}
-          variant='contained'
+          variant="contained"
           onClick={() => setOpenTeamCreator(true)}
           loading={creatingTeam}
         >
-          {t('Create new team')}
+          {t("Create new team")}
         </LoadingButton>
       </Box>
       <TeamCreate
@@ -50,12 +76,20 @@ const CollapseElement = ({ t, organization, setSelectedTeam, onTeamCreate }) => 
   );
 };
 
-const PermissionCreate = ({ open, setOpen, loading, setLoading, onCreate, treeitem = null, coproductionprocess = null }) => {
+const PermissionCreate = ({
+  open,
+  setOpen,
+  loading,
+  setLoading,
+  onCreate,
+  treeitem = null,
+  coproductionprocess = null,
+}) => {
   const [organizations, setOrganizations] = useState([]);
   const [loadingOrganizations, setLoadingOrganizations] = useState(false);
   const [selectedTeam, setSelectedTeam] = useState(null);
   const [selectedTreeItem, setSelectedTreeItem] = useState(null);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [permissions, setPermissions] = useState(initial);
   const mounted = useMounted();
   const { t } = useTranslation();
@@ -64,7 +98,7 @@ const PermissionCreate = ({ open, setOpen, loading, setLoading, onCreate, treeit
     setLoading(true);
     const dataToSend = {
       team_id: selectedTeam.id,
-      ...permissions
+      ...permissions,
     };
     if (treeitem) {
       dataToSend.treeitem_id = selectedTreeItem.id;
@@ -73,17 +107,20 @@ const PermissionCreate = ({ open, setOpen, loading, setLoading, onCreate, treeit
       dataToSend.coproductionprocess_id = coproductionprocess.id;
     }
     console.log(dataToSend);
-    permissionsApi.create(dataToSend).then((res) => {
-      setLoading(false);
-      handleClose();
-      setSelectedTeam(null);
-      if (onCreate) {
-        onCreate(res.data);
-      }
-    }).catch((err) => {
-      console.log(err);
-      setLoading(false);
-    });
+    permissionsApi
+      .create(dataToSend)
+      .then((res) => {
+        setLoading(false);
+        handleClose();
+        setSelectedTeam(null);
+        if (onCreate) {
+          onCreate(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   };
 
   const getOrganizations = () => {
@@ -103,9 +140,12 @@ const PermissionCreate = ({ open, setOpen, loading, setLoading, onCreate, treeit
   useEffect(() => {
     let delayDebounceFn;
     if (open && mounted.current) {
-      delayDebounceFn = setTimeout(() => {
-        getOrganizations();
-      }, searchValue ? 800 : 0);
+      delayDebounceFn = setTimeout(
+        () => {
+          getOrganizations();
+        },
+        searchValue ? 800 : 0
+      );
     }
     return () => {
       if (delayDebounceFn) {
@@ -120,23 +160,44 @@ const PermissionCreate = ({ open, setOpen, loading, setLoading, onCreate, treeit
 
   const another = t;
 
-  const repeated = selectedTeam && (selectedTreeItem ? selectedTreeItem.permissions.find((el) => el.team_id === selectedTeam.id && el.treeitem_id === selectedTreeItem.id) !== undefined : coproductionprocess.enabled_permissions.find((el) => el.team_id === selectedTeam.id && el.coproductionprocess_id === coproductionprocess.id && !el.treeitem_id) !== undefined);
+  const repeated =
+    selectedTeam &&
+    (selectedTreeItem
+      ? selectedTreeItem.permissions.find(
+          (el) =>
+            el.team_id === selectedTeam.id &&
+            el.treeitem_id === selectedTreeItem.id
+        ) !== undefined
+      : coproductionprocess.enabled_permissions.find(
+          (el) =>
+            el.team_id === selectedTeam.id &&
+            el.coproductionprocess_id === coproductionprocess.id &&
+            !el.treeitem_id
+        ) !== undefined);
 
   return (
     <>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        fullWidth
-        maxWidth='md'
-      >
-        <DialogTitle sx={{ bgcolor: 'background.default' }}>
-          {!selectedTeam ? t('Select the team to apply the permission') : coproductionprocess ? t('Select the permissions for the overall coproduction process for the team {{team_name}}', { team_name: selectedTeam && selectedTeam.name }) : t('Select the permissions for {{treeitem_name}} and for the team {{team_name}}', { treeitem_name: selectedTreeItem && selectedTreeItem.name, team_name: selectedTeam && selectedTeam.name })}
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
+        <DialogTitle sx={{ bgcolor: "background.default" }}>
+          {!selectedTeam
+            ? t("Select the team to apply the permission")
+            : coproductionprocess
+            ? t(
+                "Select the permissions for the overall coproduction process for the team {{team_name}}",
+                { team_name: selectedTeam && selectedTeam.name }
+              )
+            : t(
+                "Select the permissions for {{treeitem_name}} and for the team {{team_name}}",
+                {
+                  treeitem_name: selectedTreeItem && selectedTreeItem.name,
+                  team_name: selectedTeam && selectedTeam.name,
+                }
+              )}
           <IconButton
-            aria-label='close'
+            aria-label="close"
             onClick={() => setOpen(false)}
             sx={{
-              position: 'absolute',
+              position: "absolute",
               right: 8,
               top: 8,
               color: (theme) => theme.palette.grey[500],
@@ -145,111 +206,111 @@ const PermissionCreate = ({ open, setOpen, loading, setLoading, onCreate, treeit
             <Close />
           </IconButton>
         </DialogTitle>
-        <DialogContent sx={{ bgcolor: 'background.default', minHeight: '50vh' }}>
+        <DialogContent
+          sx={{ bgcolor: "background.default", minHeight: "50vh" }}
+        >
           {!selectedTeam && (
-          <OrganizationsList
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            organizations={organizations}
-            loading={loadingOrganizations}
-            getCollapseElement={(organization) => (
-              <CollapseElement
-                t={t}
-                organization={organization}
-                setSelectedTeam={setSelectedTeam}
-                onTeamCreate={(team) => {
-                  getOrganizations();
-                  setSelectedTeam(team);
-                }}
-              />
-            )}
-          />
+            <OrganizationsList
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              organizations={organizations}
+              loading={loadingOrganizations}
+              getCollapseElement={(organization) => (
+                <CollapseElement
+                  t={t}
+                  organization={organization}
+                  setSelectedTeam={setSelectedTeam}
+                  onTeamCreate={(team) => {
+                    getOrganizations();
+                    setSelectedTeam(team);
+                  }}
+                />
+              )}
+            />
           )}
 
           {selectedTeam && (
-          <>
-            <Card sx={{ p: 1 }}>
-              <CardHeader
-                avatar={(
-                  <Avatar
-                    alt={t('Team logotype')}
-                    src={selectedTeam.logotype_link}
-                    variant='rounded'
-                  >
-                    {selectedTeam.name}
-                  </Avatar>
-)}
-                title={selectedTeam.name}
-                subheader={selectedTeam.description}
-              />
-            </Card>
-            <UsersList
-              size='small'
-              disableContainer={false}
-              users={selectedTeam.users}
-              showLastLogin={false}
-            />
-
-            {!repeated ? Object.keys(permissions).map((key) => (
-              <Stack
-                key={key}
-                sx={{ mt: 3 }}
-                spacing={1}
-                direction='row'
-                alignItems='center'
-              >
-                <Switch
-                  checked={permissions[key]}
-                  onChange={(event) => setPermissions({
-                    ...permissions,
-                    [key]: event.target.checked
-                  })}
+            <>
+              <Card sx={{ p: 1 }}>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      alt={t("Team logotype")}
+                      src={selectedTeam.logotype_link}
+                      variant="rounded"
+                    >
+                      {selectedTeam.name}
+                    </Avatar>
+                  }
+                  title={selectedTeam.name}
+                  subheader={selectedTeam.description}
                 />
-                <Typography variant='body2'>{another(key)}</Typography>
+              </Card>
+              <UsersList
+                size="small"
+                disableContainer={false}
+                users={selectedTeam.users}
+                showLastLogin={false}
+              />
 
-              </Stack>
-            )) : (
-              <Alert
-                severity='error'
-                sx={{ mt: 2 }}
-              >
-                {t('There is already a permission for this team')}
-              </Alert>
-            )}
-          </>
+              {!repeated ? (
+                Object.keys(permissions).map((key) => (
+                  <Stack
+                    key={key}
+                    sx={{ mt: 3 }}
+                    spacing={1}
+                    direction="row"
+                    alignItems="center"
+                  >
+                    <Switch
+                      checked={permissions[key]}
+                      onChange={(event) =>
+                        setPermissions({
+                          ...permissions,
+                          [key]: event.target.checked,
+                        })
+                      }
+                    />
+                    <Typography variant="body2">{another(key)}</Typography>
+                  </Stack>
+                ))
+              ) : (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                  {t("There is already a permission for this team")}
+                </Alert>
+              )}
+            </>
           )}
-
         </DialogContent>
-        <DialogActions sx={{ bgcolor: 'background.default' }}>
+        <DialogActions sx={{ bgcolor: "background.default" }}>
           <MobileStepper
-            variant='dots'
+            variant="dots"
             steps={2}
-            position='static'
+            position="static"
             activeStep={selectedTeam ? 1 : 0}
             sx={{ flexGrow: 1 }}
-            nextButton={(
+            nextButton={
               <LoadingButton
-                size='small'
+                size="small"
                 onClick={handleSubmit}
                 disabled={!selectedTeam || repeated}
                 loading={loading}
               >
-                {selectedTeam ? t('Create') : t('Next')}
+                {selectedTeam ? t("Create") : t("Next")}
                 <KeyboardArrowRight />
               </LoadingButton>
-)}
-            backButton={(
+            }
+            backButton={
               <Button
-                size='small'
+                size="small"
                 onClick={() => setSelectedTeam(null)}
                 disabled={!selectedTeam}
               >
                 <KeyboardArrowLeft />
-                {t('Back')}
+                {t("Back")}
               </Button>
-            )}
+            }
           />
-
         </DialogActions>
       </Dialog>
     </>
