29bf7c8c17d693bd2fa2b6424d6da63b927ddd98
Felipe Vergara <fvergaracl@gmail.com>
2023-03-29 10:25:57 +0200

    fix some details in cypress

--- react/cypress.config.js
+++ react/cypress.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require("cypress");
-const { GoogleSocialLogin } = require("cypress-social-logins").plugins;
 const secrets = require("./src/.secrets.json");
 
 module.exports = defineConfig({
   viewportWidth: 1100,
   viewportHeight: 1000,
   env: {
-    cypress_email_login: secrets.cypress_email_login,
-    cypress_email_password: secrets.cypress_email_password,
+    cypress_email_login: secrets?.cypress_email_login,
+    cypress_email_password: secrets?.cypress_email_password,
   },
   e2e: {
     setupNodeEvents(on, config) {},
